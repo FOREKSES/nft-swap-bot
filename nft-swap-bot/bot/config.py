9a0859aa35ec17ec1e9aa0485229d418c252efee
FOREKSES <153909981+FOREKSES@users.noreply.github.com>
2025-05-30 22:37:09 +0300

    Update config.py

--- nft-swap-bot/bot/config.py
+++ nft-swap-bot/bot/config.py
@@ -1,4 +1,8 @@
-BOT_TOKEN = "8156265494:AAFg8E7mUMNQMddeDSoRxc5ICCDK2WQ0Eik"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 COMMISSION_PERCENT = 0.02  # 2% комиссия
 MIN_COMMISSION = 0.5       # Минимум 0.5 TON
 SURIKATISS_ACCOUNT = "@surikatisss"
